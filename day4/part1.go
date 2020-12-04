fcb744f3c93fd202a260d0e092ef44d428bb1a0b
kisinga <kamana@kisinga.family>
2020-12-04 16:39:29 +0300

    finishe dpart1

--- day4/part1.go
+++ day4/part1.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"strings"
 )
 
-func main() {
-	f, err := os.Open("input.txt")
+var mandatoryKeys []string = []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
+func main() {
+	content, err := ioutil.ReadFile("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer f.Close()
-
+	passports := strings.Split(string(content), "\n\n")
+	validCount := 0
+	for _, passport := range passports {
+		if validatePassport(passport) {
+			validCount++
+		}
+	}
+	fmt.Println(validCount)
+}
+func validatePassport(passport string) bool {
+	fields := strings.Fields(passport)
+	found := map[string]bool{}
+	valid := true
+	for _, field := range fields {
+		key := strings.Split(field, ":")[0]
+		found[key] = true
+	}
+	for _, key := range mandatoryKeys {
+		if !found[key] {
+			valid = false
+		}
+	}
+	return valid
 }
