e706afd476781851649fb61ce36d840c0765d8b1
kisinga <kamana@kisinga.family>
2020-12-09 00:41:28 +0300

    finished day8 part2

---
+++ day8/part2.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type operation int
+
+const (
+	acc operation = iota
+	jmp operation = iota
+	nop operation = iota
+)
+
+func (o operation) String() string {
+	return string(o)
+}
+
+type instruction struct {
+	operation
+	value int
+}
+type line struct {
+	executed bool
+	instruction
+}
+
+var accumulator int
+
+func main() {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	// make a map so that it's easy to search, just in case
+	lines := []line{}
+	for scanner.Scan() {
+		lines = append(lines, extractLine(scanner.Text()))
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for i, _ := range lines {
+		accumulator = 0
+		copy := append([]line{}, lines...)
+		switchOp(&copy[i].operation)
+		if err := run(0, &copy[0], copy); err == nil {
+			break
+		}
+	}
+
+	fmt.Println(accumulator)
+}
+func switchOp(op *operation) {
+	switch *op {
+	case nop:
+		*op = jmp
+		return
+	case jmp:
+		*op = nop
+		return
+	default:
+		return
+	}
+}
+func extractLine(l string) line {
+	parts := strings.Fields(l)
+	val, err := strconv.Atoi(parts[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	op, err := parseOperation(parts[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	return line{
+		executed: false,
+		instruction: instruction{
+			operation: op,
+			value:     val,
+		},
+	}
+}
+func run(index int, l *line, lines []line) error {
+	newIndex, err := executeLine(index, l)
+	if newIndex == len(lines) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return run(newIndex, &lines[newIndex], lines)
+}
+
+func parseOperation(op string) (operation, error) {
+	switch op {
+	case "acc":
+		return acc, nil
+	case "jmp":
+		return jmp, nil
+	case "nop":
+		return nop, nil
+	default:
+		return 0, errors.New("invalid string provided")
+	}
+}
+
+func executeLine(index int, l *line) (int, error) {
+	if l.executed {
+		return 0, errors.New("line already executed")
+	}
+	return executeInstruction(index, l), nil
+}
+
+func executeInstruction(index int, l *line) int {
+	l.executed = true
+	switch l.operation {
+	case acc:
+		accumulator += l.value
+		return index + 1
+	case jmp:
+		return index + l.value
+	default:
+		return index + 1
+	}
+}
