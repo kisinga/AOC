9df764552f2af5f27c55d6b12005423afc13ad16
Steve Kisinga <stevekisinga@microsoft.com>
2023-03-02 18:53:05 +0300

    minor update for day1

---
+++ 2022/day01/README.md
@@ -0,0 +1,11 @@
+Santa's reindeer typically eat regular reindeer food, but they need a lot of magical energy to deliver presents on Christmas. For that, their favorite snack is a special type of star fruit that only grows deep in the jungle. The Elves have brought you on their annual expedition to the grove where the fruit grows.
+
+To supply enough magical energy, the expedition needs to retrieve a minimum of fifty stars by December 25th. Although the Elves assure you that the grove has plenty of fruit, you decide to grab any fruit you see along the way, just in case.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end up with the following list:
--- 2022/day01/main.go
+++ 2022/day01/main.go
@@ -1,23 +1,74 @@
 package day01
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
 
 type day struct {
-	// data
+	// store any necessary configs here
+	dir string
 }
 
-func prepare() *day {
+// run the day solution independently
+func main() {
+	d, err := prepare("data.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = d.solve()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+func prepare(dir string) (*day, error) {
 	// read data from disk
-	return &day{}
+	return &day{dir}, nil
 }
 
-func (*day) solve() {
-	// solve the problem
-	fmt.Println("Hello from mycommand!")
+func (d *day) solve() error {
+	// read data from disk
+	file, err := os.Open(d.dir)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	highestElf := 0
+	currentElf := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			// found empty line, stop reading
+
+			if currentElf > highestElf {
+				highestElf = currentElf
+			}
+			// reset currentElf to 0
+			currentElf = 0
+			continue
+		}
+		currenLine, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		currentElf += currenLine
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+		return err
+	}
+	fmt.Println("Highest elf:", highestElf)
+	return nil
 }
 
 func Runner() *cobra.Command {
@@ -25,12 +76,22 @@ func Runner() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "1",
 		Short: "Day 1 solution",
+		
 		PreRun: func(cmd *cobra.Command, args []string) {
-			d = prepare()
+			err := error(nil)
+			d, err = prepare("day01/data.txt")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		},
 
 		Run: func(cmd *cobra.Command, args []string) {
-			d.solve()
+			err := d.solve()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 		},
 	}
 
